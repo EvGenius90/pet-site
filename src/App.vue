59b76befa03717eeec33388407fd44bd6aee6ff6
Евгений <kuzmenko.evgeniy2011@yandex.ru>
2024-06-03 12:21:16 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <Header/>
+
+  <Footer/>
+</template>
+
+<script>
+import Footer from './components/Footer'
+import Header from './components/Header'
+
+
+export default {
+  components: { Header, Footer },
+}
+</script>
+
+<style>
+
+</style>
