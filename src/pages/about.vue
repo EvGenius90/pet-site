c4f80a947febd2e52d9229c69d6cfe6c801b9ef4
Евгений <kuzmenko.evgeniy2011@yandex.ru>
2024-07-18 19:38:25 +0300

    feat: add content

--- src/pages/about.vue
+++ src/pages/about.vue
@@ -1,5 +1,64 @@
 <template>
-    <h1>from about</h1>
+    <div class="container">
+        <div class="about">
+            <img class="about__image" src="../assets/imgs/Frame 483.png" alt="">
+            <div class="about__tabs tabs">
+                <nav class="tabs__items">
+                    <a class="tabs__item" href="#tab_01"><span>Команда магазина</span></a>
+                    <a class="tabs__item" href="#tab_02"><span>Команда ветклиники</span></a>
+                </nav>
+                <div class="tabs__body">
+                    <div id="tab_01" class="tabs__block">
+                        <img src="../assets/imgs/Frame 485.png" alt="">
+                        <img src="../assets/imgs/Frame 486.png" alt="">
+                        <img src="../assets/imgs/Frame 487.png" alt="">
+                        <img src="../assets/imgs/Frame 488.png" alt="">
+                        <img src="../assets/imgs/Frame 489.png" alt="">
+                        <img src="../assets/imgs/Frame 490.png" alt="">
+                        <img src="../assets/imgs/Frame 491.png" alt="">
+                        <img src="../assets/imgs/Frame 492.png" alt="">
+                        <img src="../assets/imgs/Frame 493.png" alt="">
+                        <img src="../assets/imgs/Frame 494.png" alt="">
+                        <img src="../assets/imgs/Frame 495.png" alt="">
+                        <img src="../assets/imgs/Frame 496.png" alt="">
+                        <img src="../assets/imgs/Frame 497.png" alt="">
+                        <img src="../assets/imgs/Frame 498.png" alt="">
+                        <img src="../assets/imgs/Frame 499.png" alt="">
+                        <img src="../assets/imgs/Frame 501.png" alt="">
+                        <img src="../assets/imgs/Frame 502.png" alt="">
+                        <img src="../assets/imgs/Frame 503.png" alt="">
+                    </div>
+                    <div id="tab_02" class="tabs__block">
+                        <img src="../assets/imgs/Frame 485.png" alt="">
+                        <img src="../assets/imgs/Frame 486.png" alt="">
+                        <img src="../assets/imgs/Frame 487.png" alt="">
+                        <img src="../assets/imgs/Frame 488.png" alt="">
+                        <img src="../assets/imgs/Frame 489.png" alt="">
+                    </div>
+                </div>
+                <div class="about__information">
+                    <div class="about__characteristics">
+                        <p><span>7 лет</span> <br> работаем для вас</p>
+                        <p><span>8000</span> <br> Постоянных <br> клиентов</p>
+                        <p><span>2 часа</span> <br> Среднее время доставки</p>
+                        <p><span>17</span> <br> Неравнодушных к зверятам сотрудников</p>
+                    </div>
+                    <div class="about__history">
+                        <h2>Как мы появились</h2>
+                        <ul>
+                            «При посещении множества зоомагазинов города, я столкнулся с проблемой дефицита в Тюмени лучших брендов. Из 20 видов можно купить в лучшем случае 5 или 6. С доставкой из других городов масса проблем: долгая и платная доставка,
+                            "заморочки" транспортными компаниями,
+                            истекающие сроки годности.
+                            Это подтолкнуло меня к открытию магазина зоотоваров, который смог бы закрывать все те проблемы, с которыми я сам столкнулся. Сейчас здесь есть все, чего хотел я сам:
+                            <li>огромное разнообразие кормов от лучших в мире производителей</li>
+                            <li>бесплатная доставка по городу и возможность самовывоза из розничной точки.</li>
+                            <li>гибкая система лояльности, благодаря которой мы завоевали огромную популярность среди покупателей»</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
