9b779d3c4f4080e53e9b2a9eddaced63d5d99f78
Евгений <kuzmenko.evgeniy2011@yandex.ru>
2024-08-01 19:34:15 +0300

    feat: add content

---
+++ src/components/UI/basketComp.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="block-basket" v-for="item of items" :key="item.id">
+        <div class="block-basket__content">
+            
+        </div>
+    </div>
+</template>
+
+<script>
+import items from '@/components/seeders/items'
+import itemsAlias from '@/pages/_itemAlias'
+
+export default {
+    data(){
+        return{
+            items
+        }
+    },
+    components:{
+        itemsAlias
+    },
+}
+</script>
+
+<style>
+
+</style>
